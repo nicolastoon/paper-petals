fcdad29d300ace79f6ae55ef023913a4bb5090f3
nicolastoon <nicolastoon05@gmail.com>
2026-01-15 13:56:54 -0800

    fixing landing page banner buttons

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -6,7 +6,7 @@ const banners = [
     title: "thoughtfully crafted",
     desc: "the beautiful colors and flowers can be seen from afar, but every precise fold, crease, and bend make it elaborate and genuine",
     buttonText: "view gallery",
-    buttonLink: "./gallery/",
+    buttonLink: "#/gallery",
     imgLink: "./img/flower-banner.png",
     mobileImg: ""
   },
@@ -15,7 +15,7 @@ const banners = [
     title: "made to last forever",
     desc: "literally. no withering, no molding, no seasonal changes. just don’t water them.",
     buttonText: "commission now",
-    buttonLink: "./commission/",
+    buttonLink: "#/commission",
     imgLink: "./img/recollections-banner.png",
     mobileImg: "./img/recollections-banner-2.png"
   },
